7338b7131a45402c2ca56e7d145d329595f0d453
github username <github email address>
2017-09-23 16:30:39 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :match_word, :words
+
+  def initialize(match_word)
+    @match_word = match_word
+  end
+
+  def match(words_to_match)
+    words_to_match.select do |word|
+      word.split("").all? do |char|
+        word.count(char) == match_word.count(char)
+      end
+    end
+  end
+end
